83188f7632b0176b23b03ce5b45a16ac29269a83
Gosti <yannick.gosteli@petzi.ch>
2021-06-06 15:45:39 +0200

    fix urls import

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-import myapp
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp', include(myapp.urls)),
+    path('myapp', include('myapp.urls')),
 ]

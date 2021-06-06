56c46a185b1b6264570cc13858c53d0d3afd2cc7
Gosti <yannick.gosteli@petzi.ch>
2021-06-06 13:53:05 +0200

    reinstall-packages.sh - use no-cache

--- reinstall-packages.sh
+++ reinstall-packages.sh
@@ -3,4 +3,4 @@
 # this allow to see the output of `pip install`
 
 pip uninstall -y -r requirements.txt
-pip install -r requirements.txt -vvv --no-binary :none: --exists-action w  --src /pip-src/
+pip install -r requirements.txt -vvv --no-binary :none: --exists-action w  --src /pip-src/ --no-cache
